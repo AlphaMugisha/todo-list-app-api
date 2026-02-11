a52ea8421d629c7e46c228c650b6408c4037370b
AlphaMugisha <tigerkev07@gmail.com>
2026-02-11 14:35:04 +0200

    ade

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.json()); // to allow json files that will be sent from the client
 
 app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
 
 let tasks = [
